eeeed2072476052b26f3c86303f3d0205378e0f8
JakeZalesny <jrzalesny@gmail.com>
2022-05-07 12:08:14 -0700

    Calendar final project changes

--- Calendar_Program.py
+++ Calendar_Program.py
@@ -1,8 +1,33 @@
 """
 Calendar Program
+1. Name
+    Jake Zalesny
+
+2. Assignment Name: 
+    Lab 03: Calendar
+
+3. Assignment Description: 
+    A monthly calendar based on the users input
+
+4. What was the hardest part? Be as specific as possible. 
+    It was difficult for me to figure out how to exactly 
+    configure the display so that all the numbers would 
+    pop up correctly
+
+5. How long did it take for you to complete the assignment? 
+    It took me about 2-2:30 hours in total to complete
 
 """
 
+
+BEGINNING_SPACING = "   "
+
+"""
+MONTH LIST
+
+Args: None
+Rtrn: Returns a list of each month of the year
+"""
 def month_list():
     month_list = [
         "January",
@@ -20,10 +45,16 @@ def month_list():
     ]
     return month_list
 
+"""
+MONTH TO DAY
+
+Args: None
+Rtrns: Returns a dictionary of each month of the year and an associated number value for the days
+"""
 def month_to_day():
     month_to_day = {
         "January":31, 
-        "February":(28,29),
+        "February":28,
         "March":31, 
         "April":30, 
         "May":31, 
@@ -38,17 +69,54 @@ def month_to_day():
 
     return month_to_day
 
+"""
+GET MONTH NUMBER
+
+Args: None
+Rtrns: Returns User input for the desired month number
+"""
 def get_month_number() -> int:
     month_number = int(input("Enter a month number: "))
     return month_number
 
+"""
+GET YEAR
+
+Args: None
+Rtrns: Returns User input for the desired year
+"""
 def get_year() -> int:
     year = int(input("Enter a year number: "))
     return year
 
-def is_leap_year(year, month_to_day):
-    pass
+"""
+IS LEAP YEAR
 
+Args: Year: User inputed year. 
+Rtrns: Returns True or False based on the calculation of the leap year
+"""
+def is_leap_year(year: int, month_to_day: dict):
+    if year % 4 == 0: 
+        return True
+    
+    elif year % 4 == 0 and year % 100 == 0 :
+        return True
+    
+    elif year % 400 == 0 :
+        return True
+    
+    else :
+        return False
+
+
+"""
+COMPUTE OFFSET
+
+Args: year: User inputed year, month_to_day: dictionary containing months and associated
+day values, month_number: User inputed month, month_list: List of months in the year 
+
+Rtrns: Offset (The day the month starts on)
+"""
 def compute_offset(year: int, month_to_day: dict, month_number: int, month_list: list):
     offset = 0
     day_total = 0
@@ -65,70 +133,105 @@ def compute_offset(year: int, month_to_day: dict, month_number: int, month_list:
             break
         
         elif is_leap_year(year, month_to_day) and month == "February" :
-            month_total += month_to_day[month][2]
+            month_total += month_to_day[month] + 1
         
         else : 
-            month_total += month_to_day[month]
+            month_total += int(month_to_day[month])
     
     total = month_total + day_total
 
     offset = total % 7
 
-    print(offset)
+    return offset - 1
 
-    return offset
+"""
+DISPLAY CALENDAR
 
-def month_number_to_name(month_number):
-    if month_number == 1: 
-        month_name = "January"
-    
-    elif month_number == 2:
-        month_name = "February"
-    
-    elif month_number == 3:
-        month_name = "March"
-    
-    elif month_number == 4:
-        month_name = "April"
-    
-    elif month_number == 5:
-        month_name == "May"
-    
-    elif month_number == 6:
-        month_name = "June"
-    
-    elif month_number == 7:
-        month_name = "July"
-    
-    elif month_number == 8: 
-        month_name = "August"
-    
-    elif month_number == 9:
-        month_name = "September"
-    
-    elif month_number == 10: 
-        month_name = "October"
-    
-    elif month_number == 11:
-        month_name = "November"
-    
-    elif month_name == 12:
-        month_name = "December"
-    
-    else: 
-        month_name = "ERROR invalid month entry"
-    
-    return month_name
+Args: year: User inputed year, month_to_day: dictionary containing months and associated
+day values, month_number: User inputed month, month_list: List of months in the year 
 
-def display_calendar(year, month_to_days, offset):
-    pass
+Rtrns: None, only displays
+"""
+def display_calendar(year: int, month_to_days: dict, offset: int, month_number: int, month_list: list):
+    counter = 0
+    print(f"{BEGINNING_SPACING}Su  Mo  Tu  We  Th  Fr  Sa")
+    print(f"{BEGINNING_SPACING}", end="")
+    new_line_counter = 0
+    
+    match offset: 
+        case 0 :
+            print(" ", end="")
+        
+        case 1 :
+            print("     ", end="")
+            new_line_counter = 1
+        
+        case 2: 
+            print("         ", end="")
+            new_line_counter = 2
+        
+        case 3:
+            print("             ", end="")
+            new_line_counter = 3
+        
+        case 4: 
+            print("                 ", end="")
+            new_line_counter = 4
+        
+        case 5:
+            print("                     ", end="")
+            new_line_counter = 5
+        
+        case 6:
+            print("                         ", end="")
+            new_line_counter = 6
+        
+        case 7:
+            print("                             ", end="")
+            new_line_counter = 7
+    
+    month = month_list[month_number - 1]
+    day_count = month_to_days[month]
+    
+    if is_leap_year(year, month_to_days) and month == "February":
+        day_count = month_to_days[month] + 1
+    
+    for i in range(1, day_count + 1) :
+        if i <= 8 :
+            print(f"{i}   ", end="")
+        
+        if i >= 9 :
+            print(f"{i}  ", end="")
+        
+        new_line_counter += 1
+        #1
+        # print(new_line_counter, end=" ")
+        if new_line_counter == 7 and i >= 10 :
+            print()
+            print("   ", end="")
+            new_line_counter = 0
+        
+        elif new_line_counter == 7 :
+            print()
+            print("    ", end="")
+            new_line_counter = 0
+        
+    return       
 
+"""
+MAIN
+
+Args: None
+Rtrns None
+Calls each function and runs the program
+"""
 def main():
     month_to_days = month_to_day()
+    month = month_list()
     month_number = get_month_number()
     year = get_year()
-    offset = compute_offset(year, month_to_days, month_number)
-    display_calendar(year, month_to_days, offset)
+    offset = compute_offset(year, month_to_days, month_number, month)
+    display_calendar(year, month_to_days, offset, month_number, month)
 
 main()
 
