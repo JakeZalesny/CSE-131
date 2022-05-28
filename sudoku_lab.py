b9ca7550576fb7544b8890590ef4b072dcde11d1
JakeZalesny <jrzalesny@gmail.com>
2022-05-28 15:24:39 -0700

    sudoku final changes

--- sudoku_lab.py
+++ sudoku_lab.py
@@ -2,7 +2,7 @@
 # 1. Name:
 #      Jake Zalesny
 # 2. Assignment Name:
-#      Lab 05 : Sudoku Draft
+#      Lab 05 : Sudoku Final
 # 3. Assignment Description:
 #      So far, its just meant to put a value in a square in a sudoku game
 # 4. What was the hardest part? Be as specific as possible.
@@ -15,7 +15,6 @@
 """
 
 import json
-from lib2to3.pytree import convert
 HARD_FILE = "131.05.Hard.json"
 MEDIUM_FILE = "131.05.Medium.json"
 EASY_FILE = "131.05.Easy.json"
@@ -60,26 +59,104 @@ def coordinate_dictionary():
     }
     return coordinate_dictionary
 
-def convert_column_to_value(user_square: str) -> str:
-    column = user_square[0].upper()
+def convert_column_to_value(user_square: str, coordinate_dictionary: dict) -> str:
+    if user_square[0].upper() not in coordinate_dictionary.keys() :
+        column = user_square[1].upper()
+    else :
+        column = user_square[0].upper()
     return column
 
-def convert_row_to_int(user_square: str) -> int:
-    row = int(user_square[1]) - 1
+def convert_row_to_int(user_square: str, coordinate_dictionary: dict) -> int:
+    if user_square[1].upper() in coordinate_dictionary.keys():
+        row = int(user_square[0]) - 1
+    
+    else :
+        row = int(user_square[1]) - 1
     return row
 
 def convert_column_to_int(column: str, coordinate_dictionary: dict) -> int: 
     column_number = coordinate_dictionary[column]
     return column_number
 
-def insert_value(coordinate_dictionary: dict, column: str, row: int, column_number: int, user_value: int, board: list):
-    is_valid = True
-    while is_valid: 
-        is_valid = check_column(column, coordinate_dictionary)
-        is_valid = check_row(row)
+def insert_value(row: int, column_number: int, user_value: int, board: list, valid: bool, tile: bool):
+    if valid == True and tile == True:
         board[row][column_number] = user_value
+    else :
+        pass
 
-def possible_values(occupied_column_list: list, occupied_row_list: list):
+def check_square(column_number: int, row: int, user_value: int, board: list):
+    occupied_square_list = []
+    match column_number :
+        case 0 :
+            c_upper_bound = 2
+            c_lower_bound = 0
+        case 1 :
+            c_upper_bound = 2 
+            c_lower_bound = 0
+        case 2 : 
+            c_upper_bound = 2
+            c_lower_bound = 0
+        case 3 :
+            c_upper_bound = 5
+            c_lower_bound = 3
+        case 4 :
+            c_upper_bound = 5
+            c_lower_bound = 3
+        case 5 :
+            c_upper_bound = 5
+            c_lower_bound = 3
+        case 6 :
+            c_upper_bound = 8
+            c_lower_bound = 6
+        case 7 :
+            c_upper_bound = 8
+            c_lower_bound = 6
+        case 8 : 
+            c_upper_bound = 8
+            c_lower_bound = 6
+    
+    match row :
+        case 0 :
+            r_upper_bound = 2
+            r_lower_bound = 0
+        case 1 :
+            r_upper_bound = 2 
+            r_lower_bound = 0
+        case 2 : 
+            r_upper_bound = 2
+            r_lower_bound = 0
+        case 3 :
+            r_upper_bound = 5
+            r_lower_bound = 3
+        case 4 :
+            r_upper_bound = 5
+            r_lower_bound = 3
+        case 5 :
+            r_upper_bound = 5
+            r_lower_bound = 3
+        case 6 :
+            r_upper_bound = 8
+            r_lower_bound = 6
+        case 7 :
+            r_upper_bound = 8
+            r_lower_bound = 6
+        case 8 : 
+            r_upper_bound = 8
+            r_lower_bound = 6
+
+    for i in range(c_lower_bound, c_upper_bound):
+        if board[row][i] != " " :
+            occupied_square_list.append(i)
+    
+    for j in range(r_lower_bound, r_upper_bound):
+        if  board[i][column_number] != " ":
+            occupied_square_list.append(j)
+    
+    return occupied_square_list
+
+
+
+def possible_values(occupied_column_list: list, occupied_row_list: list, occupied_square_list: list):
     occupied_list = []
     possible_values = []
 
@@ -87,7 +164,12 @@ def possible_values(occupied_column_list: list, occupied_row_list: list):
         occupied_list.append(i)
     
     for i in occupied_column_list :
-        occupied_list.append(i)
+        if i not in occupied_list :
+            occupied_list.append(i)
+    
+    for i in occupied_square_list :
+        if i not in occupied_list :
+            occupied_list.append(i)
     
     for value in range(10) :
         if value not in occupied_list :
@@ -108,14 +190,14 @@ def check_column(column: str, coordinate_dictionary: dict) -> bool:
 # Checks the row to see if the inserted row is valid 
 def check_row(row: int) -> bool:
     if row > 9 :
+        print("ERROR: Invalid Row! Please try again.")
         return False
     
     else :
-        print("ERROR: Invalid Row! Please try again.")
         return True
 
 # Checks to see if the square is empty or not
-def check_square_for_value(row: int, column_number: int, board: list) -> bool:
+def check_tile_for_value(row: int, column_number: int, board: list) -> bool:
     if board[row][column_number] != " ":
         print("ERROR: Square already has a value! please try again.")
         return False
@@ -132,33 +214,25 @@ def check_unique_row(row: int, column_number: int, board: list, user_value: int)
     
     return occupied_number_list_column
 
-# Compares all numbers in the row to user's input
-def is_occupied_row(occupied_number_list_row: list, user_value: int) -> bool:
-    for i in occupied_number_list_row :
-        if user_value == i :
-            return False
-        
-        else :
-            return True
 
 # Returns a list of numbers in the column 
 def check_unique_column(column_number: int, board: list, user_value: int) :
     occupied_number_list_column = []
-    for i in range(8):
+    for i in range(9):
         if board[i][column_number] != " ":
             occupied_number_list_column.append(board[i][column_number])
-        
-        return occupied_number_list_column
+      
+    return occupied_number_list_column
 
-# Compares to list of numbers in the column 
-def is_occupied_column(occupied_number_list_column: list, user_value: int) -> bool:
-    for i in occupied_number_list_column :
-        if user_value == i :
-            return False
 
-        else :
-            return True
-
+def valid_value(possible_values: list, user_value: int):
+    if user_value in possible_values :
+        return True
+    
+    else :
+        print("ERROR: Invalid value!")
+        print(f"HINT these are the possible values for that square: \n{possible_values}")
+        return False
 
 def get_user_square(board, filename):
     user_square = input("Specify a coordinate to edit or \'Q\' to save and quit\n> ")
@@ -168,11 +242,11 @@ def get_user_square(board, filename):
     else :
         return user_square
 
-def get_value(user_square: str) -> int: 
+def get_value(row: int, column: str) -> int: 
     user_value = 0
     
     while user_value <= 0 or user_value >= 10 :
-        user_value = int(input(f"What number goes in {user_square}? "))
+        user_value = int(input(f"What number goes in {column.upper()}{row + 1}? "))
         
         if user_value <= 0 or user_value >= 10 :
             print("ERROR: Invalid Value, please enter a number from 1 to 9.")
@@ -259,21 +333,24 @@ def main():
     board, filename = read_file()
     user_square = None
     value_is_valid = True
-    while user_square != "Q" and value_is_valid == True:
+    while user_square != "Q":
         display_board(board)
         _coordinate_dictionary = coordinate_dictionary()
         user_square = get_user_square(board, filename)
-        user_value = get_value(user_square)
-        value_is_valid = True
-        column = convert_column_to_value(user_square)
-        row = convert_row_to_int(user_square)
+        column = convert_column_to_value(user_square, _coordinate_dictionary)
+        row = convert_row_to_int(user_square, _coordinate_dictionary)
+        user_value = get_value(row, column)
+        value_is_valid = check_row(row)
+        value_is_valid = check_column(column, _coordinate_dictionary)
         column_number = convert_column_to_int(column, _coordinate_dictionary)
+        tile_is_valid = check_tile_for_value(row, column_number, board)
         occupied_number_list_column = check_unique_column(column_number, board, user_value)
         occupied_number_list_row = check_unique_row(row, column_number, board, user_value)
-        value_is_valid = is_occupied_column(occupied_number_list_column, user_value)
-        value_is_valid = is_occupied_row(occupied_number_list_row, user_value)
-        value_is_valid = check_square_for_value(row, column_number, board)
-        insert_value(_coordinate_dictionary, column, row, column_number, user_value, board)
+        occupied_square_list = check_square(column_number, row, user_value, board)
+        _possible_values = possible_values(occupied_number_list_column, occupied_number_list_row, occupied_square_list)
+        value_is_valid = valid_value(_possible_values, user_value)
+        insert_value(row, column_number, user_value, board, value_is_valid, tile_is_valid)
+
     save_board(board, filename)
 
 main()
